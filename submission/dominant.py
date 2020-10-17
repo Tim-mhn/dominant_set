727163cf9e980b884b551bc99070aa1c7ba9c896
Meehan Timothy <timothy.meehan@student.ecp.fr>
2020-10-17 18:29:24 +0200

    Added deeper lookup with even candidates in glutton algo with MAX_DEPTH variable. Increases computation time by 3-5 and doesnt yield better results. Preferable set value to 0

--- submission/dominant.py
+++ submission/dominant.py
@@ -1,135 +1,15 @@
 import sys, os, time
 import networkx as nx
+import logging
 from dfs_dominant import dfs_dominant
 from dominant_glouton import dominant_glouton
-# from topk_glutton import topk_glutton
+from topk_glutton import topk_glutton
 
-KTOP = 5 # Pick the ktop best new nodes and search deeper
+LOG_LEVEL = logging.INFO
 
-def topk_glutton(g):
-    """
-        A Faire:         
-        - Ecrire une fonction qui retourne le dominant du graphe non dirigé g passé en parametre.
-        - cette fonction doit retourner la liste des noeuds d'un petit dominant de g
+logging.basicConfig(format='%(message)s',level=LOG_LEVEL)
+logger = logging.getLogger(__name__)
 
-        :param g: le graphe est donné dans le format networkx : https://networkx.github.io/documentation/stable/reference/classes/graph.html
-
-    """
-    empty_D = list()
-
-    list_D = [empty_D] # List of tuples (u,v) where u=subset, v = number of isolated nodes
-
-    dominant = find_dominant(list_D, g)
-    while not dominant:
-        subsets = list()
-        for D in list_D:
-            # Get k best subset from D. Add them to list of all subsets
-            subsets += _topk_subsets(D, g)
-            # #print(f"Subsets: {subsets}")
-            #print(f"{len(subsets)} Subsets: {subsets}")
-
-        # Sort subsets by number of isolated nodes (ascending order)
-        try:
-            subsets = sorted(subsets, key=lambda tup: tup[1])
-        except Exception:
-            #print(f"Subsets: {subsets}")
-            raise ValueError("e")
-        # Keep the TOPK best subsets (with least number of isolated nodes)
-        subsets = subsets[:KTOP]
-        list_D = list(map(lambda s: list(s[0]), subsets))
-        dominant = find_dominant(list_D, g)
-
-    # #print(f"Finished with {len(D)} with {D}")
-    return dominant
-
-def find_dominant(list_D, g):
-    """
-    Check if one of the subset in list_D is dominant.
-    Return first dominant if it exists
-    Return false otherwise
-    """
-    #print(f"List_D: {list_D}")
-    for D in list_D:
-        if is_dominant(D, g):
-            return D
-    return False
-
-def _topk_subsets(D, g):
-    notin_D = set(g.nodes) - set(D)
-    #print(f"Ntin D : {notin_D}")
-    # Subset candidates created from D
-    D_candidates = list(map(lambda n : D + [n], notin_D))
-    #print(f"D_candidates : {list(D_candidates)}")
-    # List of tuples (u,v) with u: subset candidate / v: number of iso nodes
-    D_candidates = list(map(lambda D_cand: (D_cand, len(get_iso_nodes(D_cand, g))), D_candidates))
-    # # #print(f"D_candidates : {list(D_candidates)}")
-    # Sort and keep KTOP best subsets
-    D_candidates = sorted(D_candidates, key=lambda tup: tup[1])
-    topk_subsets = D_candidates[:KTOP]
-    #print(f"topk_subsets {topk_subsets}")
-    # Return only 1st element of tuple = subset (and not number of iso nodes)
-    return topk_subsets
-
-def is_dominant(D, g):
-    try:
-        for n in set(g.nodes) - set(D):
-            if not edge_node_graph(n, D, g):
-                return False
-    except TypeError:
-        #print(f"Error caused by D : {D}")
-        raise ValueError("error")
-    return True
-
-def edge_node_graph(n, D, g):
-    """Checks whether there is an edge between subgraph D of g and node n"""
-    for D_node in D:
-        if g.has_edge(n, D_node):
-            return True
-    return False
-
-def min_isolated_nodes(D, g):
-    """
-    Returns next node to add to D to minimize number of isolated nodes from D
-    """
-    iso_nodes = get_iso_nodes(D, g)
-    max_new_nodes = 0
-    new_node = None
-
-    for n in set(g.nodes) - set(D):
-        new_nodes = count_new_nodes(n, g, iso_nodes)
-
-        if new_nodes > max_new_nodes:
-            new_node = n
-        # elif new_nodes == max_new_nodes:
-            # #print(f"Same new connections for {n} and {new_node}")
-
-    return new_node
-
-def get_iso_nodes(D, g):
-    """
-    Get list of isolated nodes from sub-ensemble D of graph g
-    """
-    iso_nodes = list()
-
-    for n in set(g.nodes) - set(D):
-        connected = False
-        for D_node in D:
-            if g.has_edge(n, D_node):
-                connected = True
-                break
-
-        if not connected:
-            iso_nodes.append(n)
-
-    return iso_nodes
-
-def count_new_nodes(root, g, nodes):
-    """ Count number of nodes connected to node n in g"""
-    new_connections = 0
-    for n in nodes:
-        if g.has_edge(n, root):
-            new_connections += 1
-    return new_connections
 
 
 # def arg_max_new_connections(D, g):
@@ -138,10 +18,11 @@ def count_new_nodes(root, g, nodes):
 #### Ne pas modifier le code suivant ####
 #########################################
 if __name__=="__main__":
+    total_count = 0
     start = time.time()
     input_dir = os.path.abspath(sys.argv[1])
     output_dir = os.path.abspath(sys.argv[2])
-    
+
     # un repertoire des graphes en entree doit être passé en parametre 1
     if not os.path.isdir(input_dir):
 	    print(input_dir, "doesn't exist")
@@ -151,26 +32,31 @@ if __name__=="__main__":
     if not os.path.isdir(output_dir):
 	    print(input_dir, "doesn't exist")
 	    exit() 
-	
+
     # fichier des reponses depose dans le output_dir et annote par date/heure
     output_filename = 'answers_{}.txt'.format(time.strftime("%d%b%Y_%H%M%S", time.localtime()))             
     output_file = open(os.path.join(output_dir, output_filename), 'w')
 
     for i, graph_filename in enumerate(sorted(os.listdir(input_dir))):
-            # print(f"------------------ {graph_filename}-------------------- \r\n\r\n")
+        print(f"------------------ {graph_filename}-------------------- \r\n\r\n")
+        graph_start = time.time()
         # importer le graphe
         g = nx.read_adjlist(os.path.join(input_dir, graph_filename))
         # calcul du dominant
         D = sorted(topk_glutton(g), key=lambda x: int(x))
-        # print(f"Result: {len(D)} : {D}")
+        print(f"Result: {len(D)}  |  D={D}")
+
+        if "100" in graph_filename:
+            total_count += len(D)
 
         # ajout au rapport
         output_file.write(graph_filename)
         for node in D:
             output_file.write(' {}'.format(node))
         output_file.write('\n')
-        # print(f"------------------ END OF Step {i+1}-------------------- \r\n\r\n")
+        graph_dur = time.time() - graph_start
+        logger.info(f"Graph {i+1} completed in {graph_dur}s\r\n\r\n")
 
     end = time.time()
-    # print(f"Total time : {end-start} s")
+    print(f"Total count: {total_count} Total time : {end-start} s")
     output_file.close()
--- submission/topk_glutton.py
+++ submission/topk_glutton.py
@@ -6,11 +6,18 @@ Eg: if there are several nodes that connect as many new connections, search deep
 import sys, os, time
 import networkx as nx
 import itertools
-KTOP = 5 # Pick the ktop best new nodes and search deeper
+import logging
+KTOP = 6 # Pick the ktop best new nodes and search deeper
+MAX_DEPTH = 0
+LOG_LEVEL = logging.INFO
+
+logging.basicConfig(format='%(message)s',level=LOG_LEVEL)
+logger = logging.getLogger(__name__)
+
 
 def topk_glutton(g):
     """
-        A Faire:         
+        A Faire:
         - Ecrire une fonction qui retourne le dominant du graphe non dirigé g passé en parametre.
         - cette fonction doit retourner la liste des noeuds d'un petit dominant de g
 
@@ -19,32 +26,26 @@ def topk_glutton(g):
     """
     empty_D = list()
 
-    list_D = [empty_D] # List of tuples (u,v) where u=subset, v = number of isolated nodes
+    D_subsets = [empty_D] # List of tuples (u,v) where u=subset, v = number of isolated nodes
 
-    dominant = find_dominant(list_D, g)
+    dominant = _find_dominant(D_subsets, g)
     while not dominant:
-        subsets = list()
-        for D in list_D:
+        top_candidates = list()
+        for D in D_subsets:
             # Get k best subset from D. Add them to list of all subsets
-            subsets += _topk_subsets(D, g)
-            # #print(f"Subsets: {subsets}")
-            #print(f"{len(subsets)} Subsets: {subsets}")
+            logger.debug(f"Topk candidates called in main function. D={D}")
+            top_candidates += _topk_candidates(D, g)
 
         # Sort subsets by number of isolated nodes (ascending order)
-        try:
-            subsets = sorted(subsets, key=lambda tup: tup[1])
-        except Exception:
-            #print(f"Subsets: {subsets}")
-            raise ValueError("e")
-        # Keep the TOPK best subsets (with least number of isolated nodes)
-        subsets = subsets[:KTOP]
-        list_D = list(map(lambda s: list(s[0]), subsets))
-        dominant = find_dominant(list_D, g)
+        top_candidates = sorted(top_candidates, key=lambda tup: tup[1])
 
-    # #print(f"Finished with {len(D)} with {D}")
+        top_candidates = top_candidates[:KTOP]
+        D_subsets = list(map(lambda s: list(s[0]), top_candidates))
+        dominant = _find_dominant(D_subsets, g)
+
     return dominant
 
-def find_dominant(list_D, g):
+def _find_dominant(list_D, g):
     """
     Check if one of the subset in list_D is dominant.
     Return first dominant if it exists
@@ -56,30 +57,93 @@ def find_dominant(list_D, g):
             return D
     return False
 
-def _topk_subsets(D, g):
+def _topk_candidates(D, g, depth=0, max_depth=MAX_DEPTH):
+    """
+    From a subset D, get the list of candidates=(subset, iso_nodes_count).
+    If they are even candidates, allow to look deeper in graph w.r.t MAX_DEPTH
+    """
+    logger.debug(f"_topk_subsets: depth={depth}, D={D}")
     notin_D = set(g.nodes) - set(D)
-    #print(f"Ntin D : {notin_D}")
     # Subset candidates created from D
     D_candidates = list(map(lambda n : D + [n], notin_D))
-    #print(f"D_candidates : {list(D_candidates)}")
     # List of tuples (u,v) with u: subset candidate / v: number of iso nodes
     D_candidates = list(map(lambda D_cand: (D_cand, len(get_iso_nodes(D_cand, g))), D_candidates))
-    # # #print(f"D_candidates : {list(D_candidates)}")
-    # Sort and keep KTOP best subsets
+    # Sort and keep KTOP best candidates
+    D_candidates = _keep_topk(D_candidates, g, depth, max_depth)
+    return D_candidates
+
+def _keep_topk(D_candidates, g, depth=0, max_depth=MAX_DEPTH):
+    """
+    Keep only KTOP best subset candidates.
+    If there are equal candidates on the edge of KTOP, go deeper to find best candidates
+    """
     D_candidates = sorted(D_candidates, key=lambda tup: tup[1])
-    topk_subsets = D_candidates[:KTOP]
-    #print(f"topk_subsets {topk_subsets}")
-    # Return only 1st element of tuple = subset (and not number of iso nodes)
-    return topk_subsets
+    # If we have less or equal candidates as KTOP, return all candidates
+    try:
+        edge_score = D_candidates[KTOP][1]
+    except IndexError:
+        return D_candidates
+
+    if edge_score == D_candidates[KTOP-1][1] and depth<max_depth:
+        logger.debug(f"_keep_top called. Lookup deeper with d={depth} and max_depth={max_depth}")
+        # First candidate with same edge_score
+        first = next((cand for cand in D_candidates if cand[1] == edge_score))
+
+        first_index, equal_candidates = _equal_score_candidates(edge_score, D_candidates)
+
+        logger.debug(f" {len(equal_candidates)} even candidates: {equal_candidates}")
+
+        deeper_candidates = list()
+        for cand in equal_candidates:
+            # Transform candidate (subset, #iso) into subset
+            subset = cand[0]
+            topk_candidates = _topk_candidates(subset, g, depth+1, max_depth)
+            deeper_candidates.append(topk_candidates[0])
+
+        available_spots = KTOP - first_index
+        # Sort by Isolated nodes value
+        deeper_candidates = sorted(deeper_candidates, key=lambda tup: tup[1])
+        deeper_candidates = deeper_candidates[:available_spots]
+        deeper_candidates = list(map(lambda c: list(c), deeper_candidates))
+
+        # Remove the last node that we added (since we looked-up one level deeper to decide)
+        for deeper_cand in deeper_candidates:
+            deeper_cand[0].pop()
+
+        # Top subsets are the first ones + some among the even candidates
+        topk_subsets = D_candidates[:first_index] + deeper_candidates
+        return topk_subsets
+
+    else:
+        logger.debug(f"_keep_topk entered ELSE part with {D_candidates}")
+        toreturn = D_candidates[:KTOP]
+        logger.debug(f"returning {toreturn}")
+        return toreturn
+
+
+def _equal_score_candidates(edge_score, D_candidates):
+    # Get candidates with score = edge_score 
+    # D_candidates is already sorted
+    equal_candidates =  list()
+    first_index = None
+    for idx, cand in enumerate(D_candidates):
+        if cand[1] == edge_score:
+            equal_candidates.append(cand)
+            # Index of first matching candidate
+            first_index = idx if not first_index else first_index
+
+        elif cand[1] > edge_score:
+            # As soon as we  find candidate with superior score, we return list as weve found all equal candidaates (sorted list)
+            return first_index, equal_candidates
+    return first_index, equal_candidates
+
 
 def is_dominant(D, g):
-    try:
-        for n in set(g.nodes) - set(D):
-            if not edge_node_graph(n, D, g):
-                return False
-    except TypeError:
-        #print(f"Error caused by D : {D}")
-        raise ValueError("error")
+    """ Returns if D is dominant-subset of g """
+    for n in set(g.nodes) - set(D):
+        if not edge_node_graph(n, D, g):
+            return False
+
     return True
 
 def edge_node_graph(n, D, g):
@@ -102,8 +166,6 @@ def min_isolated_nodes(D, g):
 
         if new_nodes > max_new_nodes:
             new_node = n
-        # elif new_nodes == max_new_nodes:
-            # #print(f"Same new connections for {n} and {new_node}")
 
     return new_node
 
